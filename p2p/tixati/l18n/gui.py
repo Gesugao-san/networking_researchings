c2133d85f6783f6c8f3e6f4610c681b71eca7e0b
Gesugao-san <gesugao.san@gmail.com>
2023-09-25 12:36:58 +0300

    Update l18n tools

---
+++ p2p/tixati/l18n/gui.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import tkinter as tk
+
+# Создать функцию для отображения данных в формате "Ключ: Значение"
+def display_data():
+    # Получить данные из текстовых полей
+    key = key_entry.get()
+    value = value_entry.get()
+
+    # Форматировать данные
+    formatted_data = f"Ключ: {key}, Значение: {value}"
+
+    # Очистить текстовое поле для вывода
+    output_text.delete(1.0, tk.END)
+
+    # Вывести отформатированные данные
+    output_text.insert(tk.END, formatted_data)
+
+# Создать главное окно
+root = tk.Tk()
+root.title("Пример программы с tkinter")
+
+# Создать и разместить элементы управления на главном окне
+key_label = tk.Label(root, text="Ключ:")
+key_label.pack()
+
+key_entry = tk.Entry(root)
+key_entry.pack()
+
+value_label = tk.Label(root, text="Значение:")
+value_label.pack()
+
+value_entry = tk.Entry(root)
+value_entry.pack()
+
+display_button = tk.Button(root, text="Показать данные", command=display_data)
+display_button.pack()
+
+output_text = tk.Text(root, height=5, width=30)
+output_text.pack()
+
+# Запустить цикл событий tkinter
+root.mainloop()
+
+
+
+exit(0)
+
--- p2p/tixati/l18n/parse.py
+++ p2p/tixati/l18n/parse.py
@@ -3,24 +3,70 @@
 
 
 import os
+import json
+
+data = {
+    "categories": {},
+    "comments": [],
+    "anomalies": []
+}
 
 print('go')
 
-# Открываем и читаем файл
+
 with open(os.getcwd() + '\\p2p\\tixati\\l18n\\tixati_language_template.txt', "r") as file:
     lines = file.readlines()
+#for _ in range(4):
+#    lines.append('\n')
 
-category_name = "language selection window"
+cat_name = None  # "language selection window"
 cat_i = 0
+data['categories'][cat_name] = {}
+
 i = 0
+i_max = len(lines)
 for line in lines:
     i += 1
-    line = line.strip()
-    if line.startswith("///////"):
-        print('category:', i, '+' + str(i - cat_i), line)
+    delimiter = line.strip()
+    if (i > (len(lines) - 3)):
+        print(i_max, i)
+        break
+    key = lines[i + 1].strip()
+    value = lines[i + 2].strip()
+
+    #if i == 1:
+    #    continue
+
+    if delimiter != "" and key == "":
+        continue
+
+    if key.startswith("/////// "):
+        category = key.split("/////// ")[1]
+        print('category:', i, '+' + str(i - cat_i), category)
         cat_i = i
-    elif line.startswith("//"):
-        print('comment:', i, line)
-        #continue
-    elif line.startswith("/"):
-        print('anomaly:', i, line)
+        cat_name = category
+        data['categories'][cat_name] = {}
+        continue
+    if key.startswith("// "):
+        comment = key.split("// ")[1]
+        print('comment:', i, comment)
+        #data['comments'].append(key)
+        continue
+    if key.startswith("/"):
+        print('anomaly:', i, key)
+        data['anomalies'].append(key)
+        continue
+
+    if (delimiter == '') and (key != ''):
+        #print('key: "'+str(key)+'"; value:"'+str(value)+'"')
+        data['categories'][cat_name][key] = value
+        print([cat_name, key, value])
+
+
+#print('data', data)
+print('=' * 50)
+#print('categories', json.dumps(list(data['categories'].keys()), indent=2))
+
+
+exit(0)
+
